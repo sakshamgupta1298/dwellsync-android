f3c1abd095961213476850d568d1a80313be664f
sakshamgupta18 <saksham@capitalflo.co>
2025-06-10 23:19:33 +0530

    url change

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const config = {
   },
   // Production
   production: {
-    apiUrl: 'http://192.168.1.7:3000/api', // Replace with your computer's local IP address
+    apiUrl: 'http://www.liveinsync.in:3000/api', // Replace with your DigitalOcean droplet IP or domain
   },
 };
 
