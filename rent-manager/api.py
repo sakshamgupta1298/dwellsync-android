9a96cb7ce38d7a6db0d8b68c3f86fcf95132d3a9
sakshamgupta18 <saksham@capitalflo.co>
2025-06-13 17:22:54 +0530

    changes in env file

--- rent-manager/api.py
+++ rent-manager/api.py
@@ -32,8 +32,9 @@ app.config['DEBUG'] = True  # Enable debug mode
 
 # Email configuration
 app.config['MAIL_SERVER'] = 'smtpout.secureserver.net'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
